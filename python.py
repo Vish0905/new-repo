6268c0778baaa19a037a2835e677a3c3a8d14298
Vish0905 <fnj980@gmail.com>
2026-02-25 16:28:15 +0530

    dev commit2

--- python.py
+++ python.py
@@ -1,4 +1,13 @@
 for i in range(1, 50):
     if i % 2 == 0:
         print(i)
-    
+    else:
+        print(f"{i} is odd")
+#this the program of prints the evne numbers from 1 to 50
+#this is the first job of the python progrmam
+#this the main branch 
+
+if __name__ == "__main__":
+    print("This is the main branch")
+
+#this is the third  and changes done in second branch 
